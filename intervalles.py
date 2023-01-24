2542409c9abbd571a0d0dada500391adbc01a59d
Bruno Scherrer <bruno.scherrer@inria.fr>
2023-01-24 11:49:47 +0100

    algo

--- intervalles.py
+++ intervalles.py
@@ -4,12 +4,12 @@ n=5
 
 g=[0]
 for i in range(1,n+1):
-    g.append( pow(-n,i-1) )
+    g.append( -pow(-(n+1),i-1) )
 
 print(g)
 print()
 
 for i in range(1,n+1):
-    x=g[i]+(n-1)*g[i-1]
-    y=n*g[i]
+    x = g[i]+(n-1)*g[i-1]
+    y = n*g[i]
     print(i, min(x,y),max(x,y))
