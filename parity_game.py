6235b9110705f8db586cda3f4910f64792dc9ed9
Bruno Scherrer <bruno.scherrer@inria.fr>
2023-01-24 18:29:06 +0100

    add python code

---
+++ parity_game.py
@@ -0,0 +1,285 @@
+#!/usr/bin/python3
+
+
+# code for parity game
+
+
+from random import seed, randint, sample
+from copy import deepcopy
+import numpy as np
+
+players=['EVEN/MAX','ODD/MIN '] # player 0 is Eve (wants even), player 1 is Adam (wants odd)
+
+
+class state:
+
+    def __init__(self, nb, player, next_states, val):
+        self.nb = nb
+        self.player = player
+        self.next_states = next_states
+        self.val = val
+
+    def print(self, end=''):
+        print("State",self.nb, "player:",self.player,players[self.player],"next_states:",self.next_states,"val:",self.val, end=end)
+        
+
+# class for parity and payoff games 
+        
+class game:   
+    
+    def __init__(self, nb_states, nb_actions, nb_priorities): # init a random parity game
+
+        self.states = [ state( i, #  state number,
+                               randint(0,1),   # random player
+                               [randint(0, nb_states-1) for a in range(nb_actions)],  # random next states
+                               randint(0,nb_priorities-1) )   #  priority
+                        for i in range(nb_states) ]
+
+    def print(self):
+        
+        for state in self.states:
+            state.print(end="\n")
+
+
+    # ZIELONKA ALGORITHM (reference algorithm)
+
+    def attractor(self, i, U): #  computes the attractor of set U for player i 
+        
+        oldattr = set()
+        attr = U.copy()
+
+        while( attr != oldattr ):
+
+            oldattr = attr.copy()
+            
+            for state in self.states:
+                if state.player==i:
+                    for j in state.next_states:
+                        if j in attr:
+                            attr.add(state.nb)
+                            break
+                else: # opponent
+                    test = map( lambda j: j in attr, state.next_states )
+                    if False not in test:
+                        attr.add(state.nb)
+                        
+        return attr
+
+
+    def copy_and_remove(self, A):  # remove nodes of the set A
+        
+        g = deepcopy(self)
+        
+        oldB = set()
+        B = A.copy()
+
+        while (B != oldB):
+
+            oldB = B.copy()
+            
+            for state in g.states.copy():  # (copy is important)
+                if state in g.states and state.nb in B:
+                    g.states.remove(state)
+                else:
+                    for j in B:
+                        if j in state.next_states:
+                            state.next_states.remove(j)
+                    if state.next_states == []:
+                        B.add(state.nb)      # if no more successor, add node for removal
+
+        return g
+    
+        
+    def solve_zielonka(self):  # Zielonka's algorithm
+        
+        all_priorities = set ( [ state.val for state in self.states ] )
+        all_states = set( [ state.nb for state in self.states ] )
+        
+        p = max(all_priorities)
+        i = p%2
+        
+        if p==min( [state.val for state in self.states  ] ):
+
+            W = [ set(), set() ]
+            W[i] = all_states
+            return W
+
+        U = set ( [ state.nb  for state in self.states if state.val==p ] )
+        A = self.attractor(i, U)
+        if A==all_states:
+            W = [ set(), set() ]
+            W[i] = all_states
+            return W
+
+        g2 = self.copy_and_remove(A)
+        W2 = g2.solve_zielonka()
+
+        if W2[1-i] == set():
+
+            W = [ set(), set() ]
+            W[i] = all_states
+            return W
+
+        B = self.attractor(1-i, W2[1-i])
+        if B==all_states:
+            W = [ set(), set() ]
+            W[1-i] = all_states
+            return W
+
+        g3 = self.copy_and_remove(B)
+        W3 = g3.solve_zielonka() 
+
+        W = [ set(), set() ]
+        W[i] = W3[i]
+        W[1-i] = W3[1-i].union(B)
+        return W
+
+
+
+    # ALGORITHM
+    
+    def value_iteration(self, T, verbose=False):  # (standard) Value Iteration (for a payoff game) for T steps from 0
+
+        v = dict()
+        for state in self.states:
+            v[state.nb]=0
+
+        if verbose:
+            print("Running Value Iteration")
+            
+        for t in range(T):
+
+            w = deepcopy(v)
+            
+            for state in self.states:
+
+                if state.player==0: # Even/Max
+                    y = state.next_states[0]
+                    q = float("-inf")
+                    for y2 in state.next_states:
+                        q2 = state.val + w[y2]
+                        if q2 >= q:
+                            y = y2
+                            q = q2
+                else: # Odd/Min
+                    y = state.next_states[0]
+                    q = float("inf")
+                    for y2 in state.next_states:
+                        q2 = state.val + w[y2]
+                        if q2 <= q:
+                            y = y2
+                            q = q2
+
+                v[state.nb]=q
+
+            if verbose:
+                print(t,"v=",v)
+
+        return(v)
+
+    
+    def find_winning_region(self, verbose=False):
+
+        if verbose:
+            print("Let's find a winning region for the game")
+            self.print()
+        
+        all_priorities = set ( [ state.val for state in self.states ] )
+        all_states = set( [ state.nb for state in self.states ] )
+        nb_states = len(all_states)
+        
+        p = max(all_priorities)
+        i = p%2
+
+        # build the payoff game
+        g = deepcopy(self) 
+        for state in g.states:
+            if state.val==p:
+                state.val=1-2*i
+            else:
+                state.val=0
+
+        if verbose:
+            print("One considers the following payoff game")
+            g.print()
+
+        # run n steps of value iteration
+        v = g.value_iteration(nb_states*(nb_states+1),verbose)
+
+        if verbose:
+            print("T^(n^2+n) 0 =",v)
+
+        A = set( [ state.nb for state in g.states if v[state.nb]==0 ] )
+
+        B = set( [ state.nb for state in g.states if abs(v[state.nb])<nb_states ] )
+
+        C = all_states - B
+
+        if verbose:
+            print("A=",A)
+            print("B=",B)
+            print("C=",C)
+
+        if C!=set():   # Player i wins (with parity p) on C
+            if verbose:
+                print("Found Region C=",C,"won by",players[i])
+            return C,i
+
+        if verbose:
+            print(players[i],"cannot win with priority",p)
+        
+        g2 = self.copy_and_remove( all_states - A ) # restrict the next game to A
+        return g2.find_winning_region(verbose)
+        
+
+        
+    def solve(self, verbose=False):
+
+        W = [ set(), set() ]
+        
+        g = deepcopy(self)
+
+        while len(g.states)>0:
+        
+            C,i = g.find_winning_region(verbose)
+            D = g.attractor(i,C)
+
+            W[i]=W[i].union(D)
+            g = g.copy_and_remove(D)
+            
+            if verbose:
+                print("* Region D=",D,"is won by",players[i])
+                print("Resulting game to solve")
+                g.print()
+                print()
+        
+        return(W)
+         
+
+
+
+    
+# Main: test the algorithm vs Zielonka
+
+n=4
+a=2
+d=n
+
+
+for i in range(571,10000):
+
+    seed(i)
+    g = game(n,a,d)
+    
+    print("* Seed=",i)    
+    g.print()
+    
+    W = g.solve_zielonka()
+    print("Solution (Zielonka):",W,"\n")
+    
+    W2 = g.solve(verbose=True)
+    print("Solution:",W2,"\n\n")
+    
+    if W!=W2:
+        print("STOP")
+        exit(1)
